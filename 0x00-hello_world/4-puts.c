59a5b837dfd67c3621cab9086247caa750808f7d
Nsikak <nsikakmbre@gmail.com>
2022-10-17 15:59:51 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
- /**
+/**
  *  main - Entry
- *  Return : 0
- *
+ *  Return: Always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multi lingual puzzle");
 	return (0);
 }
-
